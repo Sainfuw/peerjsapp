ec43aa53845858ff4a69f825ca3fa2bb6796b700
= <=>
2022-06-14 06:06:37 -0400

    testing peerjs for share screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react'
+import Peer from 'peerjs'
 
 function App() {
+  const [peerId, setPeerId] = useState('')
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState('')
+  const remoteVideoRef = useRef(null)
+  const peerInstance = useRef(null)
+
+  useEffect(() => {
+    const peer = new Peer()
+
+    peer.on('open', (id) => {
+      setPeerId(id)
+    })
+
+    peer.on('call', function (call) {
+      call.answer()
+      call.on('stream', function (remoteStream) {
+        remoteVideoRef.current.srcObject = remoteStream
+        remoteVideoRef.current.play()
+      })
+    })
+
+    peerInstance.current = peer
+  }, [])
+
+  const call = (remotePeerId) => {
+    const mediaOptions = {
+      video: {
+        cursor: 'always',
+        quality: 10,
+        width: { ideal: 1920 },
+        height: { ideal: 1080 },
+      },
+      audio: true,
+    }
+
+    navigator.mediaDevices
+      .getDisplayMedia(mediaOptions)
+      .then(function (mediaStream) {
+        peerInstance.current.call(remotePeerId, mediaStream)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h4>{peerId}</h4>
+      <input
+        type='text'
+        value={remotePeerIdValue}
+        onChange={(e) => setRemotePeerIdValue(e.target.value)}
+      />
+      <button onClick={() => call(remotePeerIdValue)}>Call</button>
+      <div>
+        <video ref={remoteVideoRef} controls />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
